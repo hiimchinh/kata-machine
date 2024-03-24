e0c6cde4ddcb9ba22fb6007bdbd88233eb40d003
hiimchinh <chinhhm@stringee.com>
2024-03-24 20:28:25 +0700

    solve BubbleSort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,11 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let last = arr.length; last > 0; last--) {
+        for (let j = 0; j < last; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
 }
