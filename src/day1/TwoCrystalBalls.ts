4ac0ae19a479d42381090ad3955e955217c5de88
hiimchinh <chinhhm@stringee.com>
2024-03-24 09:47:34 +0700

    add solve for TwoCrystalBalls.ts

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,15 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    const jumpStep = Math.floor(Math.sqrt(breaks.length));
+    let i = jumpStep;
+    for (; i < breaks.length; i + jumpStep) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+    for (let j = i - jumpStep; j < jumpStep; j++) {
+        if (breaks[j]) {
+            return j;
+        }
+    }
+    return -1;
 }
