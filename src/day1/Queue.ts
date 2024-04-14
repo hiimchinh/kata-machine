bdcb66c2eb6609f093d5cc76abca53b6a1226b7c
hiimchinh <chinhhm@stringee.com>
2024-04-14 21:43:31 +0700

    pass test Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,6 +1,6 @@
 type Node<T> = {
     value: T,
-    next?: Node<T>,
+    next?: Node<T>
 }
 export default class Queue<T> {
     public length: number;
@@ -9,39 +9,32 @@ export default class Queue<T> {
 
 
     constructor() {
-        this.head = this.tail = undefined;
         this.length = 0;
+        this.head = this.tail = undefined;
     }
 
     enqueue(item: T): void {
         const node = {value: item} as Node<T>;
-        if (this.length === 0) {
+        this.length++;
+        if (!this.tail) {
             this.head = this.tail = node;
             return;
         }
-        // increase length of queue
-        this.length++;
-        if (this.tail) {
-            // old tail next point to this node
-            this.tail.next = node;
-            // repoint new tail to node
-            this.tail = node;
-        }
+        this.tail.next = node;
+        this.tail = node;
     }
 
     deque(): T | undefined {
-        if (this.length === 0) {
+        if (!this.head) {
             return undefined;
         }
         this.length--;
-        if (this.length === 1) {
-            const node = this.head;
-            this.head = this.tail = undefined;
-            return node?.value;
-        }
         const node = this.head;
-        this.head = this.head?.next;
-        return node?.value;
+        this.head = this.head.next;
+        if (this.length === 0) {
+            this.tail = this.head = undefined;
+        }
+        return node.value;
     }
 
     peek(): T | undefined {
